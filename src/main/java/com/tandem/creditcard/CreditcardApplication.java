a90f5e7553a85435a1af78f6d4e1140f35914585
TANDEM6 <nijin39@gmail.com>
2018-12-26 23:58:30 +0900

    Add Repay BDD TEST

--- src/main/java/com/tandem/creditcard/CreditcardApplication.java
+++ src/main/java/com/tandem/creditcard/CreditcardApplication.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 
 @SpringBootApplication()
 @EnableBinding(Source.class)
-//@EnableScheduling
+@EnableScheduling
 public class CreditcardApplication {
 
 	private final CreditCardRepository creditCardRepository;
